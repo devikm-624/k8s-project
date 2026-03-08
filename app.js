537d071fd3599588e822aab97840a1122e42fd7f
Devi-REC <devikm.official@gmail.com>
2026-03-08 15:19:08 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const http=require("http")
+const user=process.env.DB_USER || "NO-USER"
+const password=process.env.DB_PASSWORD || "no-password"
+const message = process.env.APP_MESSAGE || "Default_Message"
+http.createServer((req,res)=>{
+    res.write(message + "\n")
+    res.write("User: " + user +"\n")
+    res.write("Password: "+password)
+    res.end()
+}).listen(3000)
+console.log("server running")
